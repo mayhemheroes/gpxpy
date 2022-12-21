0e062ae4bb0adc0939503a23f8026d17c1c04233
bcapuano <bcapuano@asu.edu>
2022-12-20 22:18:04 -0500

    Fixed no instrumentation bug

--- mayhem/fuzz_parse.py
+++ mayhem/fuzz_parse.py
@@ -5,20 +5,22 @@ import sys
 import fuzz_helpers
 
 with atheris.instrument_imports(include=['gpxpy', 'xml']):
-    import gpxpy
+    from gpxpy.parser import GPXParser
 
 from gpxpy.gpx import GPXException
 
-@atheris.instrument_func
 def TestOneInput(data):
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     if len(data) < 1:
         return -1
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     try:
-        gpx = gpxpy.parse(fdp.ConsumeRemainingString())
+        gpx_parser = GPXParser(fdp.ConsumeRemainingString())
+        gpx = gpx_parser.parse()
         gpx.to_xml()
     except GPXException:
+        return -1
+    except ValueError:
         return -1
 
 def main():
