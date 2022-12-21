ce2bdc3cd6292971200b3dd2103349cada57911e
bcapuano <bcapuano@asu.edu>
2022-12-20 22:34:42 -0500

    More

--- mayhem/fuzz_parse.py
+++ mayhem/fuzz_parse.py
@@ -10,18 +10,21 @@ with atheris.instrument_imports(include=['gpxpy', 'xml']):
 from gpxpy.gpx import GPXException
 
 def TestOneInput(data):
-    fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     if len(data) < 1:
         return -1
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     try:
         gpx_parser = GPXParser(fdp.ConsumeRemainingString())
         gpx = gpx_parser.parse()
+        gpx.get_moving_data(raw=True)
+        gpx.smooth()
         gpx.to_xml()
     except GPXException:
         return -1
-    except ValueError:
-        return -1
+    except ValueError as e:
+        if any(x in str(e) for x in ['tag', 'XML', 'URI',]):
+            return -1
+        raise
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)
